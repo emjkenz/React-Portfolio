717af11fd73c1cc3eb34ac349c749e3ffbfef1b8
Em Kennedy <36625564+emjkenz@users.noreply.github.com>
2023-07-09 01:17:46 +1000

    Added footer styling

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,17 @@
 const Footer = () => {
+    const year = new Date().getFullYear();
     return (
-        <footer>
-            <ul>
-                <li><a href="https://github.com/emjkenz">Github</a></li>
-            </ul>
-        </footer>
+        <div className="footer bg-light">
+            <footer class="py-3 my-4">
+                <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+                    <li class="nav-item">
+                        <a href="https://github.com/emjkenz" class="nav-link px-2 text-muted">Github</a>
+                    </li>
+                </ul>
+                <p class="text-center text-muted">© {year} Emily's <span className="text-success">React</span> Portfolio</p>
+            </footer>
+        </div>
+        
     );
 }
 
