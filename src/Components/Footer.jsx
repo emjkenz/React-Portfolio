c318e2b5f9d9a84bdac78985afef3134dd9e7cc5
Em Kennedy <36625564+emjkenz@users.noreply.github.com>
2023-07-09 00:35:58 +1000

    Created Footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer>
+            <ul>
+                <li><a href="https://github.com/emjkenz">Github</a></li>
+            </ul>
+        </footer>
+    );
+}
+
+export default Footer;
