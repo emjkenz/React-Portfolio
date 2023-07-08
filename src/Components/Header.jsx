038c56f0cd67d4e8e433b7c4d479c455c524334d
Em Kennedy <36625564+emjkenz@users.noreply.github.com>
2023-07-09 01:03:52 +1000

    Added nav CSS

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,25 +1,46 @@
+const isActive = (activePage, page) => {
+    return activePage === page ? ' active' : ''
+} 
+
 const Header = ({
     page,
     setPage,
 }) => {
     return (
         <header>
-            <nav>
-                <div>Logo</div>
-                <ul>
-                    <li><a
-                        href="#home"
-                        onClick={() => setPage('home')}
-                    >Home</a></li>
-                    <li><a 
-                        href="#about"
-                        onClick={() => setPage('about')}
-                    >About</a></li>
-                    <li><a 
-                        href="#other"
-                        onClick={() => setPage('other')}
-                    >Other</a></li>
-                </ul>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    {/* Logo/Name */}
+                    <div className="navbar-brand">My Portfolio</div>
+                    {/* Mobile Nav Button */}
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    {/* Links */}
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a
+                                className={`nav-link${isActive(page, 'home')}`}
+                                href="#home"
+                                onClick={() => setPage('home')}
+                            >Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a
+                                className={`nav-link${isActive(page, 'about')}`}
+                                href="#about"
+                                onClick={() => setPage('about')}
+                            >About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a
+                                className={`nav-link${isActive(page, 'other')}`}
+                                href="#other"
+                                onClick={() => setPage('other')}
+                            >Other</a>
+                        </li>
+                    </ul>
+                </div>
             </nav>
         </header>
     );
