ae8668db67dbcf470b8fac968f5725907fcc09bc
Em Kennedy <36625564+emjkenz@users.noreply.github.com>
2023-07-09 00:40:38 +1000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
