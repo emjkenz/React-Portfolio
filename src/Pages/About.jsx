9e037e18112b4042b8ddd128526b7263a9517ae5
Em Kennedy <36625564+emjkenz@users.noreply.github.com>
2023-07-09 00:36:38 +1000

    Created pages

---
+++ src/Pages/About.jsx
@@ -0,0 +1,13 @@
+const About = () => {
+    return (
+        <>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur culpa corporis 
+                ipsa consequatur delectus veritatis numquam ullam, ratione aliquam dolorum vero 
+                non voluptatem error earum quas nostrum. Rerum, dolor ipsam?
+            </p>
+        </>
+    );
+}
+
+export default About;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+const Home = () => {
+    return (
+        <>
+            <h1>Home</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur culpa corporis
+                ipsa consequatur delectus veritatis numquam ullam, ratione aliquam dolorum vero
+                non voluptatem error earum quas nostrum. Rerum, dolor ipsam?
+            </p>
+        </>
+    );
+}
+
+export default Home;
