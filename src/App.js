bcf1b23afa0816377176ca507f1edbb57d4b60b9
Em Kennedy <36625564+emjkenz@users.noreply.github.com>
2023-07-13 19:57:25 +1000

    Change the page via the url

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import 'bootstrap-icons/font/bootstrap-icons.min.css';
 import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Page from './Components/Page';
 
 function App() {
 
   const [page, setPage] = useState('home')
+
+  useEffect(() => {
+    const hash = window.location.hash.substr(1)
+    hash ? setPage(hash) : setPage('home')
+  },[])
 
   return (
     <div className="App">
