50bccc27f42ee7ef350a8dfe15a4d783972d71d7
Em Kennedy <36625564+emjkenz@users.noreply.github.com>
2023-07-13 19:58:33 +1000

    Added skills page

---
+++ src/Pages/Skills.jsx
@@ -0,0 +1,29 @@
+const Skills = () => {
+    return (
+        <div className="row">
+            <div className="col skills">
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <h2>Skills</h2>
+                            <div class="lists">
+                                <ul class="list-none">
+                                    <li>I can give the kiss of life... literally</li>
+                                    <li>If you ever need a catheter at least you will know who to go too</li>
+                                    <li>No I will not give you a prostate exam</li>
+                                </ul>
+                                <ul class="list-none">
+                                    <li>I know where to inflict pain in places you never thought of</li>
+                                    <li>If you ever need CPR and I'm drinking... that means I'm off duty. Soz.</li>
+                                    <li>I can legally stab people... with needles 👀</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Skills;
