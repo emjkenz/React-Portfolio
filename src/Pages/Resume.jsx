5b6e34afd4f221c9587305f6e745f12d5ab55275
Em Kennedy <36625564+emjkenz@users.noreply.github.com>
2023-07-13 19:58:44 +1000

    Added resume page

---
+++ src/Pages/Resume.jsx
@@ -0,0 +1,17 @@
+const Resume = () => {
+    return (
+        <div className="row">
+            <div className="col">
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Resume;
