bcbdcc2c8b7581b60b54a9c1e580e3b1df6105f3
Em Kennedy <36625564+emjkenz@users.noreply.github.com>
2023-07-13 21:17:06 +1000

    Added Resume downloader

--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -1,11 +1,16 @@
+import MyResume from '../Assets/Resume.docx';
+import './Resume.css';
+
 const Resume = () => {
     return (
         <div className="row">
             <div className="col">
                 <div className="container">
                     <div className="row">
-                        <div className="col">
-
+                        <div className="col resume">
+                            <h1>Resume</h1>
+                            <p>Download my resume</p>
+                            <a className="btn btn-primary" href={MyResume} download={'Resume.docx'}>Download <i class="bi bi-arrow-down-circle"></i></a>
                         </div>
                     </div>
                 </div>
