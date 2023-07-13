bfa2737bd63cbc43d16870b95609b9c7e1877598
Em Kennedy <36625564+emjkenz@users.noreply.github.com>
2023-07-13 22:38:08 +1000

    Fixed mobile nav

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 const isActive = (activePage, page) => {
     return activePage === page ? ' active' : ''
 }
@@ -33,18 +35,30 @@ const Header = ({
     page,
     setPage,
 }) => {
+
+    const [mobileNav, setMobileNav] = useState(false)
+
     return (
         <header>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                <div class="container-fluid">
+                <div className="container-fluid">
                     {/* Logo/Name */}
                     <div className="navbar-brand">My Portfolio</div>
                     {/* Mobile Nav Button */}
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-mdb-toggle="collapse"
+                        data-mdb-target="#navToggle"
+                        aria-controls="navbarToggleExternalContent1"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                        onClick={() => setMobileNav(!mobileNav)}
+                    >
+                        <i className="bi bi-list"></i>
                     </button>
                     {/* Links */}
-                    <ul className="navbar-nav">
+                    <ul className="navbar-nav d-none d-lg-flex">
                         {links.map(link =>
                             <li className="nav-item">
                                 <a
@@ -57,6 +71,20 @@ const Header = ({
                     </ul>
                 </div>
             </nav>
+            {/* Links */}
+            <div className={`collapse ${mobileNav ? 'show' : ''}`} id="navToggle">
+                <ul className="bg-light navbar-nav text-center">
+                    {links.map(link =>
+                        <li className="nav-item border-top">
+                            <a
+                                className={`nav-link${isActive(page, link.url)}`}
+                                href={`#${link.url}`}
+                                onClick={() => setPage(link.url)}
+                            >{link.name}</a>
+                        </li>
+                    )}
+                </ul>
+            </div>
         </header>
     );
 }
